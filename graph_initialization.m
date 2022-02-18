8952450a8e86ff52a3e029fb8ca14be29ed31628
FeeedEd <e.federici1@studenti.unipi.it>
2022-02-18 11:04:25 +0100

    I'm trying jennifer

--- graph_initialization.m
+++ graph_initialization.m
@@ -1,16 +1,18 @@
 % This method is used to generate the direct graph G = (N, A). this graph
-% is used to outline the matrix E, which is edge-node adjacency matrix of
-% G. The function requires only the number of nodes of the direct graph and
-% the number of edges, then the direct graph is randomly generated
+% is used to outline the matrix E, which is edge-node incidence matrix of
+% G. The function requires n the number of nodes of the directed graph, e 
+% the number of edges, seed an integer to generate random number and c a
+% boolean variable equal to true if the directed graph must be weakly
+% connected or equal to false otherwise.
 
-function G = graph_initialization(n, e, s)
+function G = graph_initialization(n, e, seed, c)
 
 % n is the number of nodes of the graph, while e is the number of edges
 
 % the rng determines how the rand, randi, randn and randperm functions
 % produce a sequence of random numbers. Below the default generator was
 % inserted, while the seed depends on the argument passed by the user
-rng(s, 'twister'); 
+rng(seed, 'twister'); 
 
 % creating the tails of the edges of the direct graph
 s = randi([1 n], 1, e);
@@ -18,5 +20,59 @@ s = randi([1 n], 1, e);
 % creating the heads of the edges of the direct graph
 t = randi([1 n], 1, e);
 
-% now we can create the direct graph
+% deleting buckles, we cannot generate the incidence matrix of a graph with
+% bucles, hence we have to delete them.
+
+b = false(e);
+for i = 1:e
+    if s(i) == t(i)
+        b(i) = true;
+    end
+end
+
+i = 1;
+while i <= length(b)
+    if b(i)
+        s(i) = [];
+        t(i) = [];
+        b(i) = [];
+    else
+        i = i + 1;
+    end
+end
+
+% now we delete all the duplicates among the pairs <s, t>
+A = [s' t'];
+A = unique(A, 'rows');
+s = A(:, 1)';
+t = A(:, 2)';
+
+% finally we create the directed graph
 G = digraph(s, t);
+
+% if the user has specified that the graph must be weakly connected, then
+% we add edges to the graph until this condition is satisfied
+if c == true
+    endLoop = false;
+    while(~endLoop)
+        weak_bins = conncomp(G, 'Type', 'weak');
+        startEdge = 0;
+        endEdge = 0;
+        endLoop = true;
+        for i = 1:length(weak_bins)
+            if weak_bins(i) ~= 1
+                endLoop = false;
+            end
+            if weak_bins(i) ~= startEdge && weak_bins(i) ~= endEdge
+                if startEdge == 0
+                    startEdge = i;
+                else
+                    endEdge = i;
+                    G = addedge(G, startEdge, endEdge);
+                    startEdge = 0;
+                    endEdge = 0;
+                end
+            end
+        end
+    end
+end
--- system_initialization.m
+++ system_initialization.m
@@ -9,12 +9,14 @@
 % the parameters eig_val (i.e. the eigenvalues of the diagonal matrix) and
 % dim (how many times an eigenvalue is a root of the characteristic
 % polynomial), remeber that the eigenvalues must be strictly greater than 0
-% because the matrix is positive-definite for definition
+% because the matrix is positive-definite for definition. The other
+% eigenvalues not directly specified by the user are ctreated between
+% range(0) and range(1).
 
-function [D, E, b, c] = system_initialization(G, eig_val, dim, s)
+function [D, E, b, c] = system_initialization(G, eig_val, dim, range, s)
 
-% firstly, we generate the adjacency matrix E, given the direct graph G
-E = adjacency(G);
+% firstly, we generate the incidence matrix E, given the direct graph G
+E = incidence(G);
 
 
 % the rng determines how the rand, randi, randn and randperm functions
@@ -22,44 +24,58 @@ E = adjacency(G);
 % inserted, while the seed depends on the argument passed by the user
 rng(s, 'twister');
 
-n = size(E, 1);
+n = size(E, 1); % number of nodes
+m = size(E, 2); % number of edges
 
 % Now we can intialize the vector fo the eigenvalues
-d = nan(1,n);
+d = nan(1,m);
 
 % We insert the eigenvalues passed by the user in the vector d
 j = 1;
 for i = 1:length(eig_val)
     % the matrix D is positive definite, hence its eigenvalues must be > 0
-    if (eig_val(i) > 0 & j+dim(i)-1 <= n) 
-    d(j:j+dim(i)) = eig_val(i);
-    j = j + dim(i);
+    if (eig_val(i) > 0 & j+dim(i)-1 <= m)
+        d(j:j+dim(i)) = eig_val(i);
+        j = j + dim(i);
     end
 end
 
-% The other eigenvalues not specified by the user are created randonmly in
-% the range (0, 20].
+% The other eigenvalues not specified by the user are randomly created in
+% the range from range(1) to range(2) (range is a varibale passed by the
+% user). if the user has not passed this variable to the function, the
+% eigenvalues are created between (0, 20].
 
-if (j <= n)
+if (j <= m)
     max = 20;
     min = 0.001;
+    if ~isnan(range)
+        if range(1) > 0
+            min = range(1);
+        end
+        if range(2) > 0 && range(2) > min
+            max = range(2);
+        elseif range(2) > 0 && range(2) <= min
+            max = min + 20;
+        end
+    end
     
-    d(j:n) = (max-min)*rand(n-j+1,1) + min;
+    d(j:m) = (max-min)*rand(m-j+1,1) + min;
 end
 
 % now it is possible to generate the diagonal matrix
 D = diag(d);
 
 % The last step of the algorithm consists in the creation of the vectors b
-% and c (they are created randomly).
+% and c (the vector b is created randomly while c for simplicity is the
+% null vector).
 
 max_b = 10;
 min_b = -10;
 
-max_c = 10;
-min_c = -10;
+max_c = 0;
+min_c = 0;
 
-b = (max_b-min_b)*rand(n, 1) + min_b;
+b = (max_b-min_b)*rand(m, 1) + min_b;
 c = (max_c-min_c)*rand(n, 1) + min_c;
 
 
