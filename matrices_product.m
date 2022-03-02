3dad2bd08e2b8687df6dd8a0925031107bb79bb4
Carlo <tosonicarlo99@gmail.com>
2022-03-02 17:25:36 +0100

    CG fixed

--- matrices_product.m
+++ matrices_product.m
@@ -3,4 +3,4 @@ function A = matrices_product(E, D, d)
 % This simple function is used to compute the vector ED^{-1}E'd (required
 % for the algorithm A1 (conjugate gradient).
 
-A = E*(D \ E')*d;
+A = E*(D\(E'*d));
